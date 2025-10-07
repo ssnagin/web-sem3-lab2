79110f2e199d762919532a00ded1c262dda431de
Stanislav Snagin <s_lstoo@bk.ru>
2025-10-07 23:28:12 +0300

    set up project

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,46 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path'
 
 export default defineConfig({
-    root: resolve(__dirname, 'public_html/www'),
+    root: resolve(__dirname, 'public_html/'),
+
+    server: {
+        middlewareMode: false
+    },
 
     build: {
         outDir: resolve(__dirname, 'public_html/www/dist'),
         emptyOutDir: true,
         
         rollupOptions: {
-        input: {
-            main: resolve(__dirname, 'public_html/www/index.html'),
-            // about: resolve(__dirname, 'public_html/www/about.html')
-        }
-        }
+            input: {
+                main: resolve(__dirname, 'public_html/src/ts/main.ts')
+
+                // main: resolve(__dirname, 'public_html/www/index.html'),
+                // about: resolve(__dirname, 'public_html/www/about.html')
+            },
+            output: {
+                entryFileNames: 'js/[name].js',
+                chunkFileNames: 'js/[name]-[hash].js',
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name.endsWith('.css')) {
+                        return 'css/[name].css';
+                    }
+                    return 'assets/[name].[ext]';
+                },
+            }
+        },
     },
 
-    publicDir: resolve(__dirname, 'public_html/www/assets'),
+    // publicDir: resolve(__dirname, 'public_html/www/assets'),
+    publicDir: false,
 
     resolve: {
         alias: {
         '@': resolve(__dirname, 'public_html/src'),
         '@ts': resolve(__dirname, 'public_html/src/ts'),
-        '@scss': resolve(__dirname, 'public_html/src/scss')
+        '@scss': resolve(__dirname, 'public_html/src/sass')
         }
     },
+
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
 });
