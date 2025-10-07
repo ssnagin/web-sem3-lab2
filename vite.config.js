121d91a066146256404d15fac886cbaa6f15018f
ssnagin <56646530+ssnagin@users.noreply.github.com>
2025-10-07 22:22:06 +0300

    configs in progress

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 
 export default defineConfig({
+    root: resolve(__dirname, 'public_html/www'),
 
+    build: {
+        outDir: resolve(__dirname, 'public_html/www/dist'),
+        emptyOutDir: true,
+        
+        rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'public_html/www/index.html'),
+            // about: resolve(__dirname, 'public_html/www/about.html')
+        }
+        }
+    },
+
+    publicDir: resolve(__dirname, 'public_html/www/assets'),
+
+    resolve: {
+        alias: {
+        '@': resolve(__dirname, 'public_html/src'),
+        '@ts': resolve(__dirname, 'public_html/src/ts'),
+        '@scss': resolve(__dirname, 'public_html/src/scss')
+        }
+    },
 });
